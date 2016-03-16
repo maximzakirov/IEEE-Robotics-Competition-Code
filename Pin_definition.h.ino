9004a2842c02ce5240768ff9ec937820829c3bdb
Maxim Zakirov <maxim.zakirov1@gmail.com>
2016-03-15 23:00:58 -0400

    Added files via upload

---
+++ Pin_definition.h.ino
@@ -0,0 +1,31 @@
+/*Pin definition for Yom-Yom and Pom-Pom
+  Author: Maxim Zakirov 
+  Date Created: A long long time ago in a galaxy far far away.
+  */
+
+#include <arduino.h>
+#include <newPing.h>
+  
+#ifndef Pin_definitions
+
+#define Motor1 M1
+#define Motor2 M2
+
+#define PWM1 E1
+#define PWM2 E2
+
+#define Irsignal1 A0
+#define Irsignal2 A1
+
+#define DELAY 1000
+#define LED1 2
+#define LED2 3
+
+#define ECHO 8
+#define TRIG 9
+
+#define TRIGGER_PIN  12
+#define ECHO_PIN     11
+#define MAX_DISTANCE 200
+
+#endif  
